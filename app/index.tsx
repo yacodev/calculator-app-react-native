644ea58ca5f77c58bd09867fc702cae656dc0875
yacodev <cyaco33@gmail.com>
2025-04-19 11:34:40 -0500

    eraser folders and  config initial

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const CalculatorApp = () => {
+  return (
+    <View>
+      <Text>Calcultaor</Text>
+    </View>
+  );
+};
+
+export default CalculatorApp;
